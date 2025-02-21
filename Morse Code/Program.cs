0da580650ee706a3b2a21c6edb4772d377ca79f2
Jake Scott <69864859+TheBiggestOfBoys@users.noreply.github.com>
2025-02-21 10:29:34 -0500

    simplified

--- Morse Code/Program.cs
+++ Morse Code/Program.cs
@@ -1,94 +1,149 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 
 namespace Morse_Code
 {
-    /// <summary>
-    /// The class with all the Morse code logic
-    /// </summary>
-    internal class Program
-    {
-        /// <summary>
-        /// Dictionary containing all translations.
-        /// Non-supported translations are just " ".
-        /// </summary>
-        public static readonly Dictionary<char, string> morseConversions = new()
-        {
-            {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."}, {'E', "."}, {'F', "..-."}, {'G', "--."}, {'H', "...."},
-            {'I', ".."}, {'J', ".---"}, {'K', "-.-"}, {'L', ".-.."}, {'M', "--"}, {'N', "-."}, {'O', "---"}, {'P', ".--."},
-            {'Q', "--.-"}, {'R', ".-."}, {'S', "..."}, {'T', "-"}, {'U', "..-"}, {'V', "...-"}, {'W', ".--"}, {'X', "-..-"},
-            {'Y', "-.--"}, {'Z', "--.."},
+	/// <summary>
+	/// The class with all the Morse code logic
+	/// </summary>
+	internal class Program
+	{
+		/// <summary>
+		/// Converts a character to its Morse code equivalent.
+		/// </summary>
+		/// <param name="c">The character to convert.</param>
+		/// <returns>The Morse code equivalent of the character.</returns>
+		public static string GetMorseCode(char c) => c switch
+		{
+			'A' or 'a' => ".-",
+			'B' or 'b' => "-...",
+			'C' or 'c' => "-.-.",
+			'D' or 'd' => "-..",
+			'E' or 'e' => ".",
+			'F' or 'f' => "..-.",
+			'G' or 'g' => "--.",
+			'H' or 'h' => "....",
+			'I' or 'i' => "..",
+			'J' or 'j' => ".---",
+			'K' or 'k' => "-.-",
+			'L' or 'l' => ".-..",
+			'M' or 'm' => "--",
+			'N' or 'n' => "-.",
+			'O' or 'o' => "---",
+			'P' or 'p' => ".--.",
+			'Q' or 'q' => "--.-",
+			'R' or 'r' => ".-.",
+			'S' or 's' => "...",
+			'T' or 't' => "-",
+			'U' or 'u' => "..-",
+			'V' or 'v' => "...-",
+			'W' or 'w' => ".--",
+			'X' or 'x' => "-..-",
+			'Y' or 'y' => "-.--",
+			'Z' or 'z' => "--..",
 
-            {'0', "-----"}, {'1', ".----"}, {'2', "..---"}, {'3', "...--"}, {'4', "....-"}, {'5', "....."}, {'6', "-...."},
-            {'7', "--..."}, {'8', "---.."}, {'9', "----."},
+			'0' => "-----",
+			'1' => ".----",
+			'2' => "..---",
+			'3' => "...--",
+			'4' => "....-",
+			'5' => ".....",
+			'6' => "-....",
+			'7' => "--...",
+			'8' => "---..",
+			'9' => "----.",
 
-            {'!', "-.-.--"}, {'"', ".-..-."}, {'#', " "}, {'$', "...-..-"}, {'%', " "}, {'&', ".-..."},
-            {'\'', ".----."}, {'(', "-.--."}, {')', "-.--.-"}, {'*', " "}, {'+', ".-.-."}, {',', "--..--"},
-            {'-', "-....-"}, {'.', ".-.-.-"}, {'/', "-..-."}, {':', "---..."}, {';', "-.-.-."}, {'<', " "},
-            {'=', "-...-"}, {'>', " "}, {'?', "..--.."}, {'@', ".--.-."},
+			'!' => "-.-.--",
+			'\"' => ".-..-.",
+			'#' => " ",
+			'$' => "...-..-",
+			'%' => " ",
+			'&' => ".-...",
+			'\'' => ".----.",
+			'(' => "-.--.",
+			')' => "-.--.-",
+			'*' => " ",
+			'+' => ".-.-.",
+			',' => "--..--",
+			'-' => "-....-",
+			'.' => ".-.-.-",
+			'/' => "-..-.",
+			':' => "---...",
+			';' => "-.-.-.",
+			'<' => " ",
+			'=' => "-...-",
+			'>' => " ",
+			'?' => "..--..",
+			'@' => ".--.-.",
 
-            {' ', " / "}
-        };
+			' ' => " / ",
+			_ => " "
+		};
 
-        /// <summary>
-        /// Receives the string to translate, and then displays it with the corresponding beeps
-        /// </summary>
-        static void Main()
-        {
-            // Will infinitely prompt for another translation
-            while (true)
-            {
-                Console.Write("Enter what you want to be translated (Characters like #*<>~` have no morse translation): ");
+		/// <summary>
+		/// Receives the string to translate, and then displays it with the corresponding beeps
+		/// </summary>
+		static void Main()
+		{
+			// Will infinitely prompt for another translation
+			while (true)
+			{
+				Console.Write("Enter what you want to be translated (Characters like #*<>~` have no morse translation): ");
 
-                // Check if the input is empty, and remind the user to enter something
-                string word = Console.ReadLine();
+				// Check if the input is empty, and remind the user to enter something
+				string word = Console.ReadLine();
 
-                // The StringBuilder which will be used to translate
-                StringBuilder stringBuilder = new();
+				if (string.IsNullOrWhiteSpace(word))
+				{
+					Console.WriteLine("Please enter a valid string.");
+					continue;
+				}
 
-                foreach (char c in word)
-                {
-                    // Add the translated character to the StringBuilder
-                    stringBuilder.Append(morseConversions[char.ToUpper(c)]);
-                    stringBuilder.Append(' ');
-                }
+				// The StringBuilder which will be used to translate
+				StringBuilder stringBuilder = new();
 
-                // Display the Morse translation & play the Morse beep pattern
-                PrintAndBeep(stringBuilder.ToString());
-            }
-        }
+				foreach (char c in word)
+				{
+					// Add the translated character to the StringBuilder
+					stringBuilder.Append(GetMorseCode(c));
+					stringBuilder.Append(' ');
+				}
 
-        /// <summary>
-        /// Reads the character and plays the appropriate beep
-        /// </summary>
-        /// <param name="morse">The character to read</param>
-        public static void DitDah(char morse)
-        {
-            switch (morse)
-            {
-                // If the character is a dot (dit), play the short beep
-                case '.': Console.Beep(800, 100); break;
-                // If the character is a dash (dah), play the long beep
-                case '-': Console.Beep(800, 300); break;
-                // If there is a space between characters, "pause"
-                default: Thread.Sleep(400); break;
-            }
-        }
+				// Display the Morse translation & play the Morse beep pattern
+				PrintAndBeep(stringBuilder.ToString());
+			}
+		}
 
-        /// <summary>
-        /// Print the characters and plays the corresponding beep
-        /// </summary>
-        /// <param name="morse">The string to read from</param>
-        public static void PrintAndBeep(string morse)
-        {
-            foreach (char c in morse)
-            {
-                Console.Write(c);
-                DitDah(c);
-            }
-            Console.WriteLine();
-        }
-    }
+		/// <summary>
+		/// Reads the character and plays the appropriate beep
+		/// </summary>
+		/// <param name="morse">The character to read</param>
+		public static void DitDah(char morse)
+		{
+			switch (morse)
+			{
+				// If the character is a dot (dit), play the short beep
+				case '.': Console.Beep(800, 100); break;
+				// If the character is a dash (dah), play the long beep
+				case '-': Console.Beep(800, 300); break;
+				// If there is a space between characters, "pause"
+				default: Thread.Sleep(400); break;
+			}
+		}
+
+		/// <summary>
+		/// Print the characters and plays the corresponding beep
+		/// </summary>
+		/// <param name="morse">The string to read from</param>
+		public static void PrintAndBeep(string morse)
+		{
+			foreach (char c in morse)
+			{
+				Console.Write(c);
+				DitDah(c);
+			}
+			Console.WriteLine();
+		}
+	}
 }
--- NATO Phonetic/Program.cs
+++ NATO Phonetic/Program.cs
@@ -1,85 +1,97 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Speech.Synthesis;
 
 namespace NATO_Phonetic
 {
-    internal class Program
-    {
-        /// <summary>
-        /// Dictionary containing all translations.
-        /// Non-supported translations are just " ".
-        /// </summary>
-        public static readonly Dictionary<char, string> PhoneticConversions = new()
-        {
-            {'A', "Alpha"},
-            {'B', "Bravo"},
-            {'C', "Charlie"},
-            {'D', "Delta"},
-            {'E', "Echo"},
-            {'F', "Foxtrot"},
-            {'G', "Golf"},
-            {'H', "Hotel"},
-            {'I', "India"},
-            {'J', "Juliett"},
-            {'K', "Kilo"},
-            {'L', "Lima"},
-            {'M', "Mike"},
-            {'N', "November"},
-            {'O', "Oscar"},
-            {'P', "Papa"},
-            {'Q', "Quebec"},
-            {'R', "Romeo"},
-            {'S', "Sierra"},
-            {'T', "Tango"},
-            {'U', "Uniform"},
-            {'V', "Victor"},
-            {'W', "Whiskey"},
-            {'X', "Xray"},
-            {'Y', "Yankee"},
-            {'Z', "Zulu"}
-        };
+	internal class Program
+	{
+		/// <summary>
+		/// Converts a character to its NATO phonetic alphabet equivalent.
+		/// </summary>
+		/// <param name="c">The character to convert.</param>
+		/// <returns>The NATO phonetic alphabet equivalent of the character.</returns>
+		public static string GetPhoneticCode(char c) => c switch
+		{
+			'A' or 'a' => "Alpha",
+			'B' or 'b' => "Bravo",
+			'C' or 'c' => "Charlie",
+			'D' or 'd' => "Delta",
+			'E' or 'e' => "Echo",
+			'F' or 'f' => "Foxtrot",
+			'G' or 'g' => "Golf",
+			'H' or 'h' => "Hotel",
+			'I' or 'i' => "India",
+			'J' or 'j' => "Juliet",
+			'K' or 'k' => "Kilo",
+			'L' or 'l' => "Lima",
+			'M' or 'm' => "Mike",
+			'N' or 'n' => "November",
+			'O' or 'o' => "Oscar",
+			'P' or 'p' => "Papa",
+			'Q' or 'q' => "Quebec",
+			'R' or 'r' => "Romeo",
+			'S' or 's' => "Sierra",
+			'T' or 't' => "Tango",
+			'U' or 'u' => "Uniform",
+			'V' or 'v' => "Victor",
+			'W' or 'w' => "Whiskey",
+			'X' or 'x' => "Xray",
+			'Y' or 'y' => "Yankee",
+			'Z' or 'z' => "Zulu",
+			_ => " "
+		};
 
-        /// <summary>
-        /// Receives the string to translate, and then displays it with the corresponding beeps
-        /// </summary>
-        static void Main()
-        {
-            // Will infinitely prompt for another translation
-            while (true)
-            {
-                Console.Write("Enter what you want to be spoken (only letters and numbers will be read): ");
+		/// <summary>
+		/// Receives the string to translate, and then displays it with the corresponding speech
+		/// </summary>
+		static void Main()
+		{
+			// Will infinitely prompt for another translation
+			while (true)
+			{
+				Console.Write("Enter what you want to be spoken (only letters and numbers will be read): ");
 
-                // Check if the input is empty, and remind the user to enter something
-                string messages = Console.ReadLine();
+				// Check if the input is empty, and remind the user to enter something
+				string messages = Console.ReadLine();
 
-                // Display the Morse translation & play the Morse beep pattern
-                PrintAndSpeak(messages);
-            }
-        }
+				if (string.IsNullOrWhiteSpace(messages))
+				{
+					Console.WriteLine("Please enter a valid string.");
+					continue;
+				}
 
-        /// <summary>
-        /// Print the characters and plays the corresponding beep
-        /// </summary>
-        /// <param name="message">The string to read from</param>
-        public static void PrintAndSpeak(string message)
-        {
-            SpeechSynthesizer synthesizer = new();
+				// Display the NATO phonetic translation & play the speech
+				PrintAndSpeak(messages);
+			}
+		}
 
-            string[] words = message.Split();
-            foreach (string word in words)
-            {
-                foreach (char c in word)
-                {
-                    if (char.IsLetter(c))
-                    {
-                        string phonetic = PhoneticConversions[char.ToUpper(c)];
-                        Console.Write(phonetic + ' ');
-                        synthesizer.Speak(phonetic);
-                    }
-                }
-                Console.WriteLine();
-            }
-        }
-    }
+		/// <summary>
+		/// Print the characters and plays the corresponding speech
+		/// </summary>
+		/// <param name="message">The string to read from</param>
+		public static void PrintAndSpeak(string message)
+		{
+			SpeechSynthesizer synthesizer = new();
+
+			string[] words = message.Split();
+			foreach (string word in words)
+			{
+				foreach (char c in word)
+				{
+					if (char.IsLetter(c))
+					{
+						string phonetic = GetPhoneticCode(c);
+						Console.WriteLine(phonetic);
+						synthesizer.Speak(phonetic);
+					}
+					else if (char.IsDigit(c))
+					{
+						Console.WriteLine(c);
+						synthesizer.Speak(c.ToString());
+					}
+				}
+				Console.WriteLine();
+			}
+		}
+	}
 }
